1be1dd6bf2cf3aa8465c7f2d323e3e9c7f18e7e0
olubisiayantoye <bisikingson@gmail.com>
2025-07-26 16:45:38 +0100

    modified:   week04/code/LinkedList.cs

--- week04/code/LinkedList.cs
+++ week04/code/LinkedList.cs
@@ -1,4 +1,6 @@
 using System.Collections;
+using System.Diagnostics;
+using System.Linq;
 
 public class LinkedList : IEnumerable<int>
 {
@@ -6,170 +8,239 @@ public class LinkedList : IEnumerable<int>
     private Node? _tail;
 
     /// <summary>
-    /// Insert a new node at the front (i.e. the head) of the linked list.
+    /// Inserts a new node containing <paramref name="value"/> at the beginning of the linked list.
     /// </summary>
+    /// <param name="value">The integer value to insert.</param>
     public void InsertHead(int value)
     {
-        // Create new node
         Node newNode = new(value);
-        // If the list is empty, then point both head and tail to the new node.
         if (_head is null)
         {
             _head = newNode;
             _tail = newNode;
         }
-        // If the list is not empty, then only head will be affected.
         else
         {
-            newNode.Next = _head; // Connect new node to the previous head
-            _head.Prev = newNode; // Connect the previous head to the new node
-            _head = newNode; // Update the head to point to the new node
+            newNode.Next = _head;
+            _head.Prev = newNode;
+            _head = newNode;
         }
     }
 
     /// <summary>
-    /// Insert a new node at the back (i.e. the tail) of the linked list.
+    /// Inserts a new node containing <paramref name="value"/> at the end of the linked list.
     /// </summary>
+    /// <param name="value">The integer value to insert.</param>
     public void InsertTail(int value)
     {
-        // TODO Problem 1
+        Node newNode = new(value);
+
+        if (_tail is null)
+        {
+            _head = newNode;
+            _tail = newNode;
+        }
+        else
+        {
+            _tail.Next = newNode;
+            newNode.Prev = _tail;
+            _tail = newNode;
+        }
     }
 
-
     /// <summary>
-    /// Remove the first node (i.e. the head) of the linked list.
+    /// Removes the first node in the list (the head).
     /// </summary>
     public void RemoveHead()
     {
-        // If the list has only one item in it, then set head and tail 
-        // to null resulting in an empty list.  This condition will also
-        // cover an empty list.  Its okay to set to null again.
         if (_head == _tail)
         {
             _head = null;
             _tail = null;
         }
-        // If the list has more than one item in it, then only the head
-        // will be affected.
         else if (_head is not null)
         {
-            _head.Next!.Prev = null; // Disconnect the second node from the first node
-            _head = _head.Next; // Update the head to point to the second node
+            _head.Next!.Prev = null;
+            _head = _head.Next;
         }
     }
 
-
     /// <summary>
-    /// Remove the last node (i.e. the tail) of the linked list.
+    /// Removes the last node in the list (the tail).
     /// </summary>
     public void RemoveTail()
     {
-        // TODO Problem 2
+        if (_head == _tail)
+        {
+            _head = null;
+            _tail = null;
+        }
+        else if (_tail is not null)
+        {
+            _tail.Prev!.Next = null;
+            _tail = _tail.Prev;
+        }
     }
 
     /// <summary>
-    /// Insert 'newValue' after the first occurrence of 'value' in the linked list.
+    /// Inserts <paramref name="newValue"/> after the first occurrence of <paramref name="value"/> in the list.
     /// </summary>
+    /// <param name="value">The existing value to search for.</param>
+    /// <param name="newValue">The new value to insert after the existing value.</param>
     public void InsertAfter(int value, int newValue)
     {
-        // Search for the node that matches 'value' by starting at the 
-        // head of the list.
         Node? curr = _head;
         while (curr is not null)
         {
             if (curr.Data == value)
             {
-                // If the location of 'value' is at the end of the list,
-                // then we can call insert_tail to add 'new_value'
                 if (curr == _tail)
                 {
                     InsertTail(newValue);
                 }
-                // For any other location of 'value', need to create a 
-                // new node and reconnect the links to insert.
                 else
                 {
                     Node newNode = new(newValue);
-                    newNode.Prev = curr; // Connect new node to the node containing 'value'
-                    newNode.Next = curr.Next; // Connect new node to the node after 'value'
-                    curr.Next!.Prev = newNode; // Connect node after 'value' to the new node
-                    curr.Next = newNode; // Connect the node containing 'value' to the new node
+                    newNode.Prev = curr;
+                    newNode.Next = curr.Next;
+                    curr.Next!.Prev = newNode;
+                    curr.Next = newNode;
                 }
 
-                return; // We can exit the function after we insert
+                return;
             }
 
-            curr = curr.Next; // Go to the next node to search for 'value'
+            curr = curr.Next;
         }
     }
 
     /// <summary>
-    /// Remove the first node that contains 'value'.
+    /// Removes the first node in the list that contains the specified <paramref name="value"/>.
     /// </summary>
+    /// <param name="value">The value to remove from the list.</param>
     public void Remove(int value)
     {
-        // TODO Problem 3
+        Node? curr = _head;
+
+        while (curr is not null)
+        {
+            if (curr.Data == value)
+            {
+                if (curr == _head)
+                {
+                    RemoveHead();
+                }
+                else if (curr == _tail)
+                {
+                    RemoveTail();
+                }
+                else
+                {
+                    curr.Prev!.Next = curr.Next;
+                    curr.Next!.Prev = curr.Prev;
+                }
+
+                return;
+            }
+
+            curr = curr.Next;
+        }
     }
 
     /// <summary>
-    /// Search for all instances of 'oldValue' and replace the value to 'newValue'.
+    /// Replaces all occurrences of <paramref name="oldValue"/> in the list with <paramref name="newValue"/>.
     /// </summary>
+    /// <param name="oldValue">The value to search for and replace.</param>
+    /// <param name="newValue">The value to replace <paramref name="oldValue"/> with.</param>
     public void Replace(int oldValue, int newValue)
     {
-        // TODO Problem 4
+        Node? curr = _head;
+        while (curr is not null)
+        {
+            if (curr.Data == oldValue)
+            {
+                curr.Data = newValue;
+            }
+            curr = curr.Next;
+        }
     }
 
     /// <summary>
-    /// Yields all values in the linked list
+    /// Returns an enumerator that iterates forward through the linked list.
+    /// </summary>
+    /// <returns>An enumerator for forward traversal.</returns>
+    public IEnumerator<int> GetEnumerator()
+    {
+        var curr = _head;
+        while (curr is not null)
+        {
+            yield return curr.Data;
+            curr = curr.Next;
+        }
+    }
+
+    /// <summary>
+    /// Returns a non-generic enumerator for forward traversal (required by IEnumerable).
     /// </summary>
     IEnumerator IEnumerable.GetEnumerator()
     {
-        // call the generic version of the method
         return this.GetEnumerator();
     }
 
     /// <summary>
-    /// Iterate forward through the Linked List
+    /// Returns an IEnumerable that iterates backward from the tail to the head.
     /// </summary>
-    public IEnumerator<int> GetEnumerator()
+    /// <returns>An IEnumerable for reverse traversal.</returns>
+    public IEnumerable Reverse()
     {
-        var curr = _head; // Start at the beginning since this is a forward iteration.
+        var curr = _tail;
         while (curr is not null)
         {
-            yield return curr.Data; // Provide (yield) each item to the user
-            curr = curr.Next; // Go forward in the linked list
+            yield return curr.Data;
+            curr = curr.Prev;
         }
     }
 
     /// <summary>
-    /// Iterate backward through the Linked List
+    /// Returns a string representation of the linked list.
     /// </summary>
-    public IEnumerable Reverse()
-    {
-        // TODO Problem 5
-        yield return 0; // replace this line with the correct yield return statement(s)
-    }
-
+    /// <returns>A string showing the list contents.</returns>
     public override string ToString()
     {
         return "<LinkedList>{" + string.Join(", ", this) + "}";
     }
 
-    // Just for testing.
-    public Boolean HeadAndTailAreNull()
+    /// <summary>
+    /// Checks if both head and tail are null (i.e. the list is empty).
+    /// </summary>
+    /// <returns>True if empty, false otherwise.</returns>
+    public bool HeadAndTailAreNull()
     {
         return _head is null && _tail is null;
     }
 
-    // Just for testing.
-    public Boolean HeadAndTailAreNotNull()
+    /// <summary>
+    /// Checks if both head and tail are not null (i.e. the list is not empty).
+    /// </summary>
+    /// <returns>True if both are not null, false otherwise.</returns>
+    public bool HeadAndTailAreNotNull()
     {
         return _head is not null && _tail is not null;
     }
 }
 
-public static class IntArrayExtensionMethods {
-    public static string AsString(this IEnumerable array) {
+/// <summary>
+/// Extension methods for displaying IEnumerable as formatted string.
+/// </summary>
+public static class IntArrayExtensionMethods
+{
+    /// <summary>
+    /// Returns the string representation of an IEnumerable of integers.
+    /// </summary>
+    /// <param name="array">The IEnumerable to convert to string.</param>
+    /// <returns>A formatted string.</returns>
+    public static string AsString(this IEnumerable array)
+    {
         return "<IEnumerable>{" + string.Join(", ", array.Cast<int>()) + "}";
     }
 }
